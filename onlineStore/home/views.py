ec45e3c91e57387bd9514d24ffc04d870ef5f85f
srikant19 <srikant.suman19@gmail.com>
2020-08-24 18:08:47 +0530

    changing_info_of_individual_cards
    through loop

--- onlineStore/home/views.py
+++ onlineStore/home/views.py
@@ -8,4 +8,24 @@ def home(request):
     prod1.name = 'chair'
     prod1.price = 400
     prod1.img = "chair.jpeg"
-    return render(request,'index.html',{'prod1': prod1})
+
+    
+    prod2 = product()
+    prod2.name = 'sofa'
+    prod2.price = 8000
+    prod2.img = "sofa.jpeg"
+   
+    prod3 = product()
+    prod3.name = 'bed'
+    prod3.price = 4000
+    prod3.img = "bed.jpeg"
+
+    prod4 = product()
+    prod4.name = 'wardrobe'
+    prod4.price = 2500
+    prod4.img = "wardrobe.jpeg"
+
+   
+    products = [prod1,prod2,prod3,prod4]
+
+    return render(request,'index.html',{'products': products})
