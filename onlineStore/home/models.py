e5e139a8b14dbad43866714768055e714415a303
srikant19 <srikant.suman19@gmail.com>
2020-08-24 17:33:12 +0530

    changing_data_to_webpage
    changing data through request by using class and accessing it's variables

--- onlineStore/home/models.py
+++ onlineStore/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class product:
+    name : str
+    price : int
+    img : str
--- onlineStore/home/views.py
+++ onlineStore/home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import product
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html',{'price': 800})
+    prod1 = product()
+    prod1.name = 'chair'
+    prod1.price = 400
+    return render(request,'index.html',{'prod1': prod1})
