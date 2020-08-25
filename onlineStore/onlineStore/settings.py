138adae92489bc11c29c0cc79cc6510aa40d3c41
srikant19 <srikant.suman19@gmail.com>
2020-08-26 00:40:53 +0530

    migrated_to_database(onlineStore)
    error : price not included in migration file

--- onlineStore/onlineStore/settings.py
+++ onlineStore/onlineStore/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'onlineStore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'onlineCart',
+        'NAME': 'onlineStore',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST' : 'localhost'
