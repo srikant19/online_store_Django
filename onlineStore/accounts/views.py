8eead6c4ac501c5cd377b741a2d53ba4c38e2fe7
srikant19 <srikant.suman19@gmail.com>
2020-08-26 18:36:12 +0530

    login_module_basic

--- onlineStore/accounts/views.py
+++ onlineStore/accounts/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def register(request):
-    return render(request,'register.html')
+    
+       if request.method == 'POST':
+            firstname = request.POST['firstname']
+            lastname = request.POST['lastname']
+            username = request.POST['username']
+            email = request.POST['email']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+            username = request.POST['username']
+          
+            user = User.objects.create_user(username=username,password=password1,email=email,first_name=firstname,last_name=lastname)
+            user.save()
+            return redirect('/')
+       else:
+           return render(request,'register.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
