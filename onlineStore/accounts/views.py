1c1b5e0fa4161fc7e082dc6ea4b013b929d071f4
srikant19 <srikant.suman19@gmail.com>
2020-08-26 20:10:27 +0530

    login_module_errormessage

--- onlineStore/accounts/views.py
+++ onlineStore/accounts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
 def register(request):
-    
+     
        if request.method == 'POST':
             firstname = request.POST['firstname']
             lastname = request.POST['lastname']
@@ -13,10 +14,23 @@ def register(request):
             password1 = request.POST['password1']
             password2 = request.POST['password2']
             username = request.POST['username']
-          
-            user = User.objects.create_user(username=username,password=password1,email=email,first_name=firstname,last_name=lastname)
-            user.save()
-            return redirect('/')
+
+            if password1 == password2:
+                if User.objects.filter(username=username).exists():
+                   messages.info(request,'username taken')
+                   
+                elif User.objects.filter(email=email).exists():
+                    messages.info(request,'email taken')   
+                    
+                else:
+                  user = User.objects.create_user(username=username,password=password1,email=email,first_name=firstname,last_name=lastname)
+                  user.save()
+                 
+                  
+            else:
+                print('password not matching')
+                return redirect('/')
+            
        else:
            return render(request,'register.html')
 
