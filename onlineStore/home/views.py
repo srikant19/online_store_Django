05d69d69cf2f1fbe0fc75f56e1c51dc486329c77
srikant19 <srikant.suman19@gmail.com>
2020-08-26 13:16:44 +0530

    final_db_connectivity_of_products

--- onlineStore/home/views.py
+++ onlineStore/home/views.py
@@ -4,30 +4,7 @@ from .models import product
 # Create your views here.
 
 def home(request):
-    prod1 = product()
-    prod1.name = 'chair'
-    prod1.price = 400
-    prod1.img = "chair.jpeg"
-
-    
-    prod2 = product()
-    prod2.name = 'sofa'
-    prod2.price = 8000
-    prod2.img = "sofa.jpeg"
+    products = product.objects.all()
    
-    prod3 = product()
-    prod3.name = 'bed'
-    prod3.price = 4000
-    prod3.img = "bed.jpeg"
-
-    prod4 = product()
-    prod4.name = 'wardrobe'
-    prod4.price = 2500
-    prod4.img = "wardrobe.jpeg"
-
- 
-
-   
-    products = [prod1,prod2,prod3,prod4]
 
     return render(request,'index.html',{'products': products})
